ca0248cb767ad2103b6fa4e81b133be6129e1f68
Renato Tomassetti <example@example.com>
2020-03-21 12:45:19 +0100

    new site version

--- assets/js/audioplayer.js
+++ assets/js/audioplayer.js
@@ -1,24 +1,93 @@
 function audioPlayer() {
     var currentSong = 0;
-    $("#audioPlayer")[0].src = $("#playlist li a")
-        [0];
-    $("#audioPlayer")[0].play()
+    $("#playlist li audio")[currentSong].play();
+
+    function playOnClick() {
+          currentSong = $(this).parent().index();
+          var items = $("#playlist li");
+          var songsNumber = items.length;
+          e.preventDefault();
+          $("#playlist li audio")[currentSong].play();
+          $("#playlist li").removeClass("current-song");
+          $(this).parent().addClass("current-song");
+          for (var i = 0; i < songsNumber; ++i) {
+            if(i != currentSong) {
+                $("#playlist li audio")[i].pause();
+                $("#playlist li audio")[i].currentTime = 0;
+              }
+          }
+    }
+
+    $("#playlist li audio").onplay = function() {
+      currentSong = $(this).parent().index();
+      var items = $("#playlist li");
+      var songsNumber = items.length;
+      e.preventDefault();
+      $("#playlist li audio")[currentSong].play();
+      $("#playlist li").removeClass("current-song");
+      $(this).parent().addClass("current-song");
+      for (var i = 0; i < songsNumber; ++i) {
+        if(i != currentSong) {
+            $("#playlist li audio")[i].pause();
+            $("#playlist li audio")[i].currentTime = 0;
+          }
+      }
+    };
+
     $("#playlist li a").click(function (e) {
-        e.preventDefault();
-        $("#audioPlayer")[0].src = this
-        $("#audioPlayer")[0].play();
-        $("#playlist li").removeClass("current-song");
         currentSong = $(this).parent().index();
+        var items = $("#playlist li");
+        var songsNumber = items.length;
+        e.preventDefault();
+        $("#playlist li audio")[currentSong].play();
+        $("#playlist li").removeClass("current-song");
         $(this).parent().addClass("current-song");
+        for (var i = 0; i < songsNumber; ++i) {
+          if(i != currentSong) {
+              $("#playlist li audio")[i].pause();
+              $("#playlist li audio")[i].currentTime = 0;
+            }
+        }
     });
 
-    $("#audioPlayer")[0].addEventListener("ended", function () {
+    $("#playlist #play").click(function (e) {
+        currentSong = $(this).parent().index();
+        var items = $("#playlist li");
+        var songsNumber = items.length;
+        e.preventDefault();
+        $("#playlist li audio")[currentSong].play();
+        $("#playlist li").removeClass("current-song");
+        $(this).parent().addClass("current-song");
+        for (var i = 0; i < songsNumber; ++i) {
+          if(i != currentSong) {
+              $("#playlist li audio")[i].pause();
+              $("#playlist li audio")[i].currentTime = 0;
+            }
+        }
+    });
+
+    $("#playlist #pause").click(function (e) {
+        currentSong = $(this).parent().index();
+        var items = $("#playlist li");
+        var songsNumber = items.length;
+        e.preventDefault();
+        $("#playlist li audio")[currentSong].play();
+        $("#playlist li").removeClass("current-song");
+        $(this).parent().addClass("current-song");
+        for (var i = 0; i < songsNumber; ++i) {
+          if(i != currentSong) {
+              $("#playlist li audio")[i].pause();
+              $("#playlist li audio")[i].currentTime = 0;
+            }
+        }
+    });
+
+    $("#playlist li audio")[currentSong].addEventListener("ended", function () {
         currentSong++;
         if(currentSong === $("#playlist li a").length)
             currentSong = 0;
         $("#playlist li").removeClass("current-song");
         $("#playlist li:eq("+currentSong+")").addClass("current-song");
-        $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href
-        $("#audioPlayer")[0].play()
+        $("#playlist li audio")[currentSong].play()
     })
 }
