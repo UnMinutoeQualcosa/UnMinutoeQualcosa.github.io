ba24a0e87171eeda78698dc56fb328c6a267363c
UnMinutoeQualcosa <62398535+UnMinutoeQualcosa@users.noreply.github.com>
2020-03-21 16:11:14 +0100

    Update audioplayer.js

--- assets/js/audioplayer.js
+++ assets/js/audioplayer.js
@@ -2,92 +2,35 @@ function audioPlayer() {
     var currentSong = 0;
     $("#playlist li audio")[currentSong].play();
 
-    function playOnClick() {
-          currentSong = $(this).parent().index();
-          var items = $("#playlist li");
-          var songsNumber = items.length;
-          e.preventDefault();
-          $("#playlist li audio")[currentSong].play();
-          $("#playlist li").removeClass("current-song");
-          $(this).parent().addClass("current-song");
-          for (var i = 0; i < songsNumber; ++i) {
-            if(i != currentSong) {
-                $("#playlist li audio")[i].pause();
-                $("#playlist li audio")[i].currentTime = 0;
-              }
-          }
-    }
-
-    $("#playlist li audio").onplay = function() {
-      currentSong = $(this).parent().index();
-      var items = $("#playlist li");
-      var songsNumber = items.length;
-      e.preventDefault();
-      $("#playlist li audio")[currentSong].play();
-      $("#playlist li").removeClass("current-song");
-      $(this).parent().addClass("current-song");
-      for (var i = 0; i < songsNumber; ++i) {
-        if(i != currentSong) {
-            $("#playlist li audio")[i].pause();
-            $("#playlist li audio")[i].currentTime = 0;
-          }
-      }
-    };
-
     $("#playlist li a").click(function (e) {
         currentSong = $(this).parent().index();
         var items = $("#playlist li");
         var songsNumber = items.length;
         e.preventDefault();
-        $("#playlist li audio")[currentSong].play();
-        $("#playlist li").removeClass("current-song");
-        $(this).parent().addClass("current-song");
         for (var i = 0; i < songsNumber; ++i) {
-          if(i != currentSong) {
-              $("#playlist li audio")[i].pause();
-              $("#playlist li audio")[i].currentTime = 0;
+            if (i !== currentSong) {
+                $("#playlist li audio")[i].pause();
+                $("#playlist li audio")[i].currentTime = 0;
+                $("#playlist li")[i].classList.add("paused");
+            } else if (i === currentSong && $(this).parent().prop('class').includes("paused")) {
+                $("#playlist li audio")[currentSong].play();
+                $("#playlist li").removeClass("current-song");
+                $(this).parent().addClass("current-song");
+                $(this).parent().removeClass("paused");
+                $("#playlist li audio")[currentSong].addEventListener("ended", function () {
+                    currentSong++;
+                    if (currentSong === $("#playlist li a").length)
+                        currentSong = 0;
+                    $("#playlist li").removeClass("current-song");
+                    $("#playlist li:eq(" + currentSong + ")").addClass("current-song");
+                    $("#playlist li audio")[currentSong].play()
+                });
+            } else {
+                $("#playlist li audio")[currentSong].pause();
+                $("#playlist li").removeClass("current-song");
+                $(this).parent().addClass("current-song");
+                $(this).parent().addClass("paused");
             }
         }
     });
-
-    $("#playlist #play").click(function (e) {
-        currentSong = $(this).parent().index();
-        var items = $("#playlist li");
-        var songsNumber = items.length;
-        e.preventDefault();
-        $("#playlist li audio")[currentSong].play();
-        $("#playlist li").removeClass("current-song");
-        $(this).parent().addClass("current-song");
-        for (var i = 0; i < songsNumber; ++i) {
-          if(i != currentSong) {
-              $("#playlist li audio")[i].pause();
-              $("#playlist li audio")[i].currentTime = 0;
-            }
-        }
-    });
-
-    $("#playlist #pause").click(function (e) {
-        currentSong = $(this).parent().index();
-        var items = $("#playlist li");
-        var songsNumber = items.length;
-        e.preventDefault();
-        $("#playlist li audio")[currentSong].play();
-        $("#playlist li").removeClass("current-song");
-        $(this).parent().addClass("current-song");
-        for (var i = 0; i < songsNumber; ++i) {
-          if(i != currentSong) {
-              $("#playlist li audio")[i].pause();
-              $("#playlist li audio")[i].currentTime = 0;
-            }
-        }
-    });
-
-    $("#playlist li audio")[currentSong].addEventListener("ended", function () {
-        currentSong++;
-        if(currentSong === $("#playlist li a").length)
-            currentSong = 0;
-        $("#playlist li").removeClass("current-song");
-        $("#playlist li:eq("+currentSong+")").addClass("current-song");
-        $("#playlist li audio")[currentSong].play()
-    })
 }
