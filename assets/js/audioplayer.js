3679faebe360f84111dfdfe49da80a26c4b169df
Renato Tomassetti <example@example.com>
2020-03-28 22:43:16 +0100

    remove anchors

--- assets/js/audioplayer.js
+++ assets/js/audioplayer.js
@@ -1,60 +1,60 @@
 function audioPlayer() {
 
-    $('#playlist li a span img').dblclick(function (e) {
+    $('#playlist li span img').dblclick(function (e) {
 
-        var currentSong = $(this).parent().parent().parent().index();
+        var currentSong = $(this).parent().parent().index();
         $("#playlist li audio")[currentSong].currentTime = 0;
         $("#playlist li audio")[currentSong].play();
         $("#playlist li").removeClass("current-song");
 
 
-        $(this).parent().parent().parent().addClass("current-song");
-        $("#playlist li a span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
-        $(this).parent().parent().parent().removeClass("paused");
+        $(this).parent().parent().addClass("current-song");
+        $("#playlist li span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
+        $(this).parent().parent().removeClass("paused");
         $("#playlist li audio")[currentSong].addEventListener("ended", function () {
             currentSong++;
-            if (currentSong === $("#playlist li a").length)
+            if (currentSong === $("#playlist li").length)
                 currentSong = 0;
             $("#playlist li").removeClass("current-song");
-            $("#playlist li a span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
+            $("#playlist li span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
             $("#playlist li:eq(" + currentSong + ")").addClass("current-song");
             $("#playlist li audio")[currentSong].play()
         });
     });
 
 
-    $('#playlist li a span img').click(function (e) {
-      var currentSong = $(this).parent().parent().parent().index();
+    $('#playlist li span img').click(function (e) {
+      var currentSong = $(this).parent().parent().index();
         var items = $("#playlist li");
         var songsNumber = items.length;
         e.preventDefault();
         for (var i = 0; i < songsNumber; ++i) {
-            $("#playlist li a span img")[i].setAttribute('src', 'assets/img/UMEQ-02.png');
+            $("#playlist li span img")[i].setAttribute('src', 'assets/img/UMEQ-02.png');
             if (i !== currentSong) {
                 $("#playlist li audio")[i].pause();
                 $("#playlist li audio")[i].currentTime = 0;
                 $("#playlist li")[i].classList.add("paused");
-            } else if (i === currentSong && $(this).parent().parent().parent().prop('class').includes("paused")) {
+            } else if (i === currentSong && $(this).parent().parent().prop('class').includes("paused")) {
                 $("#playlist li audio")[currentSong].play();
-                $("#playlist li a span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
+                $("#playlist li span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
                 $("#playlist li").removeClass("current-song");
-                $(this).parent().parent().parent().addClass("current-song");
-                $(this).parent().parent().parent().removeClass("paused");
+                $(this).parent().parent().addClass("current-song");
+                $(this).parent().parent().removeClass("paused");
                 $("#playlist li audio")[currentSong].addEventListener("ended", function () {
                     currentSong++;
-                    if (currentSong === $("#playlist li a").length)
+                    if (currentSong === $("#playlist li").length)
                         currentSong = 0;
                     $("#playlist li").removeClass("current-song");
-                    $("#playlist li a span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
+                    $("#playlist li span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
                     $("#playlist li:eq(" + currentSong + ")").addClass("current-song");
                     $("#playlist li audio")[currentSong].play()
                 });
             } else {
                 $("#playlist li audio")[currentSong].pause();
                 $("#playlist li").removeClass("current-song");
-                $("#playlist li a span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
-                $(this).parent().parent().parent().addClass("current-song");
-                $(this).parent().parent().parent().addClass("paused");
+                $("#playlist li span img")[currentSong].setAttribute('src', 'assets/img/UMEQ-01.png');
+                $(this).parent().parent().addClass("current-song");
+                $(this).parent().parent().addClass("paused");
             }
         }
     });
@@ -63,11 +63,11 @@ function audioPlayer() {
 
 $(document).ready(function () {
 
-    $("#playlist li a span img").mouseover(function (e) {
+    $("#playlist li span img").mouseover(function (e) {
         this.setAttribute('src', 'assets/img/UMEQ-01.png');
     });
-    $("#playlist li a span img").mouseout(function (e) {
-        if(!$(this).parent().parent().parent().prop('class').includes("current-song"))
+    $("#playlist li span img").mouseout(function (e) {
+        if(!$(this).parent().parent().prop('class').includes("current-song"))
             this.setAttribute('src', 'assets/img/UMEQ-02.png');
     });
 
